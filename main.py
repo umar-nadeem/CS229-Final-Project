0832f494f7eb1e5acaaaf874bf0cae5311a073b3
sarahraza007 <61799142+sarahraza007@users.noreply.github.com>
2021-05-25 18:01:00 -0700

    Script to get JSON data

---
+++ main.py
@@ -0,0 +1,40 @@
+# This is a sample Python script.
+import os
+import json
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def runCommands(name):
+    # Use a breakpoint in the code line below to debug your script.
+    # print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    # os.system(f"echo Hello {name}")
+    os.chdir("/Users/sarahraza/bazel")
+    # os.system("ls")
+    # print("'cd /Users/sarahraza/bazel' ran with exit code %d" % bazel_dir)
+    os.system(f"git checkout Head~1")
+    os.system(f"bazel build //src:bazel-dev --build_event_json_file=/Users/sarahraza/Desktop/Build_Info/{name}_commit.json")
+
+"""
+def readJSONtoCSV(nCommits):
+    # Use a breakpoint in the code line below to debug your script.
+    # print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    # os.system(f"echo Hello {name}")
+    os.chdir("/Users/sarahraza/Desktop/Build_Info")
+    for j in range(1, nCommits):
+        f = open(f"{j}_commit.json")
+        data = json.load(f)
+        for i in data ["id"]:
+            print(i)
+
+    f.close()
+"""
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    nCommits = 2
+    for i in range(1, nCommits):
+        runCommands(i)
+    # readJSONtoCSV(nCommits)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
